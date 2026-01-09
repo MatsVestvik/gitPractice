8cadd15ac1ad330350f4a935bbc9509613b274e5
MatsVestvik <mats.o.vestvik@gmail.com>
2026-01-09 11:32:59 +0100

    this is testing the git config file

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello, Worldd!");
-        System.out.println("this is actually written on testBranch");
     }
 }
